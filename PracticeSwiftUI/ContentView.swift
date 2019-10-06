a0a061b6788984dee42de0b72cb8eefded5163a4
tx-TEM <highhigheggman@yahoo.co.jp>
2019-10-06 21:31:20 +0900

    ボタンタップするごとに数字が増えるViewを作った

--- PracticeSwiftUI/ContentView.swift
+++ PracticeSwiftUI/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 1
+
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .center) {
+            Text("Count")
+                .font(.title)
+                .background(Color.white)
+
+            Text("\(count)")
+                .font(.system(size: 64))
+                .background(Color.white)
+                .padding(16)
+
+            Spacer()
+
+            Button(action: {
+                self.count += 1
+            }, label: {
+                Text("Click")
+                    .font(.system(size: 32))
+                    .foregroundColor(Color.white)
+                    .background(Color.gray)
+                    .padding(16)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            })
+            .background(Color.blue)
+            .padding(16)
+        }
+        .background(Color.red)
     }
 }
 
