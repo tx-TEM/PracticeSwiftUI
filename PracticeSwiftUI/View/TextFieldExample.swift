1cc8548bfba5dcd75fa548d3cb562b28879e10b7
tx-TEM <highhigheggman@gmail.com>
2019-11-04 22:48:51 +0900

    UITextFieldをラップ

--- PracticeSwiftUI/View/TextFieldExample.swift
+++ PracticeSwiftUI/View/TextFieldExample.swift
@@ -9,28 +9,37 @@
 import SwiftUI
 
 struct TextFieldExample: View {
-    @State var text = ""
+    @State var text1 = ""
+    @State var text2 = ""
     @State var secureText = ""
 
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            TextField("text field", text: $text)
+            TextField("text field", text: $text1)
                 .padding(.horizontal, 32)
-                .background(Color(UIColor.white))
                 .padding(.top, 32)
 
 
             Color(UIColor.black).frame(height: 1)
-            .padding(.horizontal, 32)
+                .padding(.horizontal, 32)
+                .padding(.bottom, 32)
+
+            Text(verbatim: text2)
+
+            UIKitTextField(text: $text2, configure: {_ in })
+                .padding(.horizontal, 32)
+
+            Color(UIColor.black).frame(height: 1)
+                .padding(.horizontal, 32)
                 .padding(.bottom, 32)
 
             SecureField("secure field", text: $secureText)
-            .padding(.horizontal, 32)
-            .background(Color(UIColor.white))
+                .padding(.horizontal, 32)
+                .background(Color(UIColor.white))
 
             Color(UIColor.black).frame(height: 1)
-            .padding(.horizontal, 32)
+                .padding(.horizontal, 32)
 
             Spacer()
 
---
+++ PracticeSwiftUI/View/UIView/UIKitTextField.swift
@@ -0,0 +1,47 @@
+import Foundation
+import SwiftUI
+
+struct UIKitTextField: UIViewRepresentable {
+    @Binding var text: String
+    let configure: (UITextField) -> ()
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        configure(textField)
+        return textField
+    }
+
+    func updateUIView(_ textView: UITextField, context: Context) {
+        textView.text = text
+    }
+}
+
+extension UIKitTextField {
+    final class Coordinator: NSObject, UITextFieldDelegate {
+        private var textField: UIKitTextField
+
+        init(_ textField: UIKitTextField) {
+            self.textField = textField
+        }
+
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            if let text = textField.text {
+                self.textField.text = text
+
+
+                // 変換中でないなら
+//                if textField.markedTextRange == nil {
+//                    self.textField.text = textField.text ?? ""
+//                } else {
+//
+//                }
+            }
+            return true
+        }
+    }
+}
